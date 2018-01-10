66e3e0d3216b311979446e8bfc2db6dd0eb36c0b
Amir <Amir.Aavani@gmail.com>
2018-01-09 23:53:31 -0800

    HTTPClient

--- Network-Units/HTTP/HTTPClientUnit.pp
+++ Network-Units/HTTP/HTTPClientUnit.pp
@@ -5,9 +5,46 @@ unit HTTPClientUnit;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, fphttpclient;
+
+type
+
+  { THTTPClient }
+
+  THTTPClient = class(TFPHTTPClient)
+  private
+    class var Client: THTTPClient;
+  public
+    function Get(const url: AnsiString; Params: TStringList): AnsiString;
+
+    class function GetClient: THTTPClient; static;
+  private
+    class procedure InitClient;
+
+  end;
 
 implementation
 
+{ THTTPClient }
+
+function THTTPClient.Get(const url: AnsiString; Params: TStringList
+  ): AnsiString;
+begin
+  Result := inherited Get(Url);
+end;
+
+class function THTTPClient.GetClient: THTTPClient;
+begin
+  Result := Client;
+end;
+
+class procedure THTTPClient.InitClient;
+begin
+  Client := THTTPClient.Create(nil);
+
+end;
+
+initialization
+  THTTPClient.InitClient;
 end.
 
