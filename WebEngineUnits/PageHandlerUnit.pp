23c65160dbb66db5fecadb68212397ee6b22df12
Amir <Amir@Aavani.net>
2020-12-15 21:32:32 -0800

    ...

--- WebEngineUnits/PageHandlerUnit.pp
+++ WebEngineUnits/PageHandlerUnit.pp
@@ -20,9 +20,46 @@ type
     function WouldHandleRequest(ARequest: THTTPServerRequest): Boolean; override;
   end;
 
+  { TXMLBasePageHandler }
+
+  TXMLBasePageHandler = class(TBasePageHandler)
+  public
+    constructor Create(aName: AnsiString; aServingPath: AnsiString);
+
+    function Execute(Sender: THTTPServerThread; TheRequest: THTTPServerRequest;
+      TheResponse : THTTPServerResponse): Boolean; override;
+    function WouldHandleRequest(ARequest: THTTPServerRequest): Boolean; override;
+  end;
+
 implementation
 uses
   httpprotocol;
+
+{ TXMLBasePageHandler }
+
+constructor TXMLBasePageHandler.Create(aName: AnsiString;
+  aServingPath: AnsiString);
+begin
+  inherited Create(aName, aServingPath);
+
+end;
+
+function TXMLBasePageHandler.Execute(Sender: THTTPServerThread;
+  TheRequest: THTTPServerRequest; TheResponse: THTTPServerResponse): Boolean;
+begin
+  TheResponse.OriginalResponse.SetHeader(hhContentType, 'text/xml; charset=utf-8');
+
+  Result := True;
+
+end;
+
+function TXMLBasePageHandler.WouldHandleRequest(ARequest: THTTPServerRequest
+  ): Boolean;
+begin
+  Result := ARequest.PathInfo = Self.ServingPath;
+
+end;
+
 { THTMLBasePageHandler }
 
 constructor THTMLBasePageHandler.Create(aName: AnsiString;
