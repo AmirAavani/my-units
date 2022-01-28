9fcdec25ffcd81e88f3c18d08e257a9750d841dd
Amir <Amir@Aavani.net>
2022-01-27 20:41:17 -0800

    TupleUnit

--- General-Purpose-Units/TupleUnit.pp
+++ General-Purpose-Units/TupleUnit.pp
@@ -1,4 +1,4 @@
-unit PairUnit;
+unit TupleUnit;
 
 {$mode ObjFPC}{$H+}
 
@@ -14,12 +14,30 @@ type
 
   end;
 
+  generic TTriplet<TFirst, TSecond, TThird> = record
+    First: TFirst;
+    Second: TSecond;
+    Third: TThird;
+
+  end;
+
   generic function MakePair<TFirst, TSecond>(First: TFirst; Second: TSecond): specialize TPair<TFirst, TSecond>;
+  generic function MakeTriplet<TFirst, TSecond, TThird>(First: TFirst; Second: TSecond; Third: TThird): specialize TTriplet<TFirst, TSecond, TThird>;
 
 implementation
 
 generic function MakePair<TFirst, TSecond>(First: TFirst; Second: TSecond): specialize TPair<TFirst, TSecond>;
 begin
+  Result.First := First;
+  Result.Second := Second;
+
+end;
+
+generic function MakeTriplet<TFirst, TSecond, TThird>(First: TFirst; Second: TSecond; Third: TThird): specialize TTriplet<TFirst, TSecond, TThird>;
+begin
+  Result.First := First;
+  Result.Second := Second;
+  Result.Third := Third;
 
 end;
 
