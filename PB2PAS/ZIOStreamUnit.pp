e495b5bf7bfc53628e9c3a5ddb63f46ed38fc56a
Amir <Amir.Aavani@gmail.com>
2026-04-20 13:36:59 -0700

    ...

--- PB2PAS/ZIOStreamUnit.pp
+++ PB2PAS/ZIOStreamUnit.pp
@@ -1,4 +1,4 @@
-unit ZioStreamUnit;
+unit ZIOStreamUnit;
 
 {$mode objfpc}{$H+}
 
@@ -8,11 +8,14 @@ uses
   Classes, SysUtils, ProtoStreamUnit, ProtoHelperUnit;
 
 type
+
   { TZioStream }
+
   TZioStream = class(TObject)
   private
     FStream: TStream;
     FOwnsStream: Boolean;
+
   public
     constructor Create(AStream: TStream; AOwnsStream: Boolean = False);
     destructor Destroy; override;
@@ -25,45 +28,47 @@ type
 
 implementation
 
-{ We define a local class to "crack" the protected section of TBaseMessage }
 type
+  { Cracker class to access protected LoadFromStream }
   TBaseMessageCracker = class(TBaseMessage);
+
+{ TZioStream }
 
 constructor TZioStream.Create(AStream: TStream; AOwnsStream: Boolean);
 begin
   inherited Create;
   FStream := AStream;
   FOwnsStream := AOwnsStream;
+
 end;
 
 destructor TZioStream.Destroy;
 begin
-  if FOwnsStream then FStream.Free;
+  if FOwnsStream then
+    FStream.Free;
+
   inherited Destroy;
+
 end;
 
 procedure TZioStream.WriteMessage(AMessage: TBaseMessage);
 var
   TempStream: TMemoryStream;
   Writer: TProtoStreamWriter;
-  // Use a string for easier assignment, or fixed array
   Header: array[0..11] of AnsiChar;
 begin
-  if AMessage = nil then Exit;
+  if AMessage = nil then
+    Exit;
 
-  // Initialize Header to avoid hints
   FillChar(Header, SizeOf(Header), #32);
   Move(PAnsiChar('ZIO1PBUF')^, Header[0], 8);
 
   TempStream := TMemoryStream.Create;
   try
-    // Use the PUBLIC TStream version to serialize
     AMessage.SaveToStream(TempStream);
 
-    // 1. Write ZIO Header
     FStream.WriteBuffer(Header[0], 12);
 
-    // 2. Write Length (Varint32)
     Writer := TProtoStreamWriter.Create(FStream, False);
     try
       Writer.WriteRawVarint32(TempStream.Size);
@@ -71,9 +76,8 @@ begin
       Writer.Free;
     end;
 
-    // 3. Write Data
-    FStream.Position := FStream.Position; // Ensure sync
     FStream.WriteBuffer(TempStream.Memory^, TempStream.Size);
+
   finally
     TempStream.Free;
   end;
@@ -87,33 +91,25 @@ var
   Magic: string[4];
 begin
   Result := False;
-  if AMessage = nil then
+  if (AMessage = nil) or (FStream.Position + 12 > FStream.Size) then
     Exit;
 
-  // Ensure we have enough data for a header
-  if FStream.Position + 12 > FStream.Size then Exit;
-
-  // 1. Read Header
   FStream.ReadBuffer(Header[0], 12);
 
-  // Validation
   SetLength(Magic, 4);
   Move(Header[0], Magic[1], 4);
   if Magic <> 'ZIO1' then
     Exit;
 
-  // 2. Read Size and Data
   Reader := TProtoStreamReader.Create(FStream, False);
   try
     MsgSize := Reader.ReadVarUInt32;
-
-    // 3. CRITICAL FIX:
-    // We cast AMessage to our Cracker class to access the
-    // protected LoadFromStream(TProtoStreamReader; Len: Integer)
     Result := TBaseMessageCracker(AMessage).LoadFromStream(Reader, MsgSize);
+
   finally
     Reader.Free;
   end;
+
 end;
 
 end.
